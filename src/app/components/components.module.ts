75703268d9b9e92ff8eb500e2eb509d9dd31a954
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2024-05-31 01:40:46 -0300

    navbar start

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
+  imports: [RouterModule, CommonModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class ComponentsModule {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  currentPath: string | undefined;
+  isMenuOpen: boolean = false;
+  constructor(private readonly _router: Router) {
+    this._router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => this.currentPath = (event as RouterEvent).url);
+  }
 }
